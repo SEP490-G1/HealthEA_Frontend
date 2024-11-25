276bda1a9306328e4d74e085900473a6feaf9391
gitzno <hoangthuy.forjob@gmail.com>
2024-11-25 08:52:41 +0700

    Clear Project

--- SEP490_HealthEA/vite.config.js
+++ SEP490_HealthEA/vite.config.js
@@ -7,13 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler' // or "modern"
-      }
-    }
-  },
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
