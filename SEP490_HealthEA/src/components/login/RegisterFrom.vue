14db209a33ffb2c6eb5d038e35649a853da22e0b
gitzno <hoangthuy.forjob@gmail.com>
2024-12-03 02:12:44 +0700

    a

--- SEP490_HealthEA/src/components/login/RegisterFrom.vue
+++ SEP490_HealthEA/src/components/login/RegisterFrom.vue
@@ -152,9 +152,13 @@ export default {
       const Response = await userStoreLogin.Register(newValue)
       if (Response.status != 200) {
         var str = 'Có lỗi xảy ra!'
-  
+        console.log(Response);
+        
         if (Response.response.data.code == 1001) {
           str = 'Tài khoản đã tồn tại'
+        }
+        if(Response.response.data.code == 1010){
+          str = 'Email không tồn tại'
         }
         message.error(str, 4)
       }
