7e5909663ddc5fe70c5009ae218aee74def406fb
gitzno <hoangthuy.forjob@gmail.com>
2024-12-10 05:53:06 +0700

    homeagpe

--- SEP490_HealthEA/src/views/HomeView.vue
+++ SEP490_HealthEA/src/views/HomeView.vue
@@ -15,15 +15,17 @@
           height: 50vh;
           display: flex;
           align-items: center;
-          justify-content: center;
+          justify-content: flex-start;
         "
       >
-        <a-typography-title style="color: aliceblue" :level="2">{{ Title }} </a-typography-title>
+        <a-typography-title style="margin-left: 200px; color: aliceblue" :level="2"
+          >{{ Title }}
+        </a-typography-title>
       </div>
 
       <a-typography-title
         style="display: flex; align-items: center; justify-content: center"
-        :level="2"
+        :level="1"
       >
         Danh sách bác sĩ nổi bật
       </a-typography-title>
@@ -39,8 +41,23 @@ export default {
     doctorList
   },
   mounted() {
-    const store = useUserStore()
-    console.log(store.user)
+    try {
+      const store = useUserStore()
+      var name = store.user.userFirstName + ' ' + store.user.userLastName
+      let now = new Date()
+      let hours = now.getHours()
+      console.log('user', hours)
+      var str = `Chào buổi sáng`
+      if (hours > 12) {
+        str = 'Chào buổi chiều'
+      }
+      if (hours > 18) {
+        str = 'Chào buổi tối'
+      }
+      this.Title = `${str} ${name},`
+    } catch   {
+      console.log('Chill day')
+    }
   },
   data() {
     return {
