5160a6574ce5b13915a95acbde04dfa9c3ca05d4
gitzno <hoangthuy.forjob@gmail.com>
2024-12-10 06:45:49 +0700

    a

--- SEP490_HealthEA/src/router/index.js
+++ SEP490_HealthEA/src/router/index.js
@@ -143,7 +143,7 @@ const routes = [
   {
     path: '/chat',
     name: 'chat',
-    component: () => import('@/components/dailymetric/ChatAI')
+    component: () => import('@/components/dailymetric/ChatDaily')
   },
   {
     path: '/callVideo',
