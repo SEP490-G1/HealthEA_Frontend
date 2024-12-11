3e8a45de6baebfdcaf646701f721f85ffb294658
gitzno <hoangthuy.forjob@gmail.com>
2024-12-12 02:56:48 +0700

    done

--- SEP490_HealthEA/vite.config.js
+++ SEP490_HealthEA/vite.config.js
@@ -8,6 +8,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
+  base:'./',
+  optimizeDeps: {},
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
